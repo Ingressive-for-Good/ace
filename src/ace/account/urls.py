21bb45fc1cf42eaa05d86d3c8c725bb0bc183f00
unknown <captainvee3@gmail.com>
2021-10-14 01:52:00 +0100

    added the links to the html files

--- src/ace/account/urls.py
+++ src/ace/account/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import register
 
 
-# urlpatterns = [
-#     path('', home, name='landing_page'),
-# ]
+urlpatterns = [
+	# path('profile/', user_views.profile, name='profile'),
+	# path('register/', user_views.register, name='register'),
+	path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+	path('signup/', register, name='signup'),
+]
+
+
