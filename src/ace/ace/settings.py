b35e02d7a41650775545b0e5f479033f192dd37e
unknown <captainvee3@gmail.com>
2021-10-27 21:27:21 +0100

    updated poetry and also worked on details view

--- src/ace/ace/settings.py
+++ src/ace/ace/settings.py
@@ -142,3 +142,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGIN_REDIRECT_URL = 'welcome'
+
+LOGIN_URL = 'login'
