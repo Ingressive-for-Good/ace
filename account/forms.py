935e91a6af04eb12c7a53e238486de9b366725f7
Victor Joseph <53542380+CaptainVee@users.noreply.github.com>
2021-10-30 14:35:24 -0700

    updated forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,16 +1,16 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 from django.db import transaction
 
 
-class UserRegistrationForm(UserCreationForm):
-	email = forms.EmailField()
+# class UserRegistrationForm(UserCreationForm):
+# 	email = forms.EmailField()
 
-	class Meta:
-		model = User
-		fields = ('username', 'email', 'password1', 'password2')
+# 	class Meta:
+# 		model = User
+# 		fields = ('username', 'email', 'password1', 'password2')
 
 class UserUpdateForm(forms.ModelForm):
 	class Meta:
